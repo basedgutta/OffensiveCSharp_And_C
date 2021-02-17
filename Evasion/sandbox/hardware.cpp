1fcc3fd788b4deb2fe38d7f2650eb142142930cb
basedgutta <71115010+basedgutta@users.noreply.github.com>
2021-02-17 18:19:44 -0500

    Create hardware.cpp

---
+++ Evasion/sandbox/hardware.cpp
@@ -0,0 +1,37 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wincrypt.h>
+#pragma comment (lib, "crypt32.lib")
+#pragma comment (lib, "advapi32")
+#include <psapi.h>
+#include "helpers.h"
+
+#pragma comment(linker, "/entry:WinMain")
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+	// check CPU
+	SYSTEM_INFO systemInfo;
+	GetSystemInfo(&systemInfo);
+	DWORD numberOfProcessors = systemInfo.dwNumberOfProcessors;
+	if (numberOfProcessors < 2) return false;
+
+	// check RAM
+	MEMORYSTATUSEX memoryStatus;
+	memoryStatus.dwLength = sizeof(memoryStatus);
+	GlobalMemoryStatusEx(&memoryStatus);
+	DWORD RAMMB = memoryStatus.ullTotalPhys / 1024 / 1024;
+	if (RAMMB < 2048) return false;
+
+	// check HDD
+	HANDLE hDevice = CreateFileW(L"\\\\.\\PhysicalDrive0", 0, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+	DISK_GEOMETRY pDiskGeometry;
+	DWORD bytesReturned;
+	DeviceIoControl(hDevice, IOCTL_DISK_GET_DRIVE_GEOMETRY, NULL, 0, &pDiskGeometry, sizeof(pDiskGeometry), &bytesReturned, (LPOVERLAPPED)NULL);
+	DWORD diskSizeGB;
+	diskSizeGB = pDiskGeometry.Cylinders.QuadPart * (ULONG)pDiskGeometry.TracksPerCylinder * (ULONG)pDiskGeometry.SectorsPerTrack * (ULONG)pDiskGeometry.BytesPerSector / 1024 / 1024 / 1024;
+	if (diskSizeGB < 100) return false;
+
+} 
