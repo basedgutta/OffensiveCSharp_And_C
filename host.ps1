7727ca1a5827790b077a3a0156f3fb1eafbc859d
King Capital <71115010+basedgutta@users.noreply.github.com>
2023-01-28 00:28:31 -0500

    Update host.ps1

--- host.ps1
+++ host.ps1
@@ -5,3 +5,23 @@ $liveHosts | Select-Object Address
 $hostname = "IP"
 $wmi = [WMIClass]"\\$hostname\root\cimv2:Win32_OperatingSystem"
 $wmi.Ping()
+
+
+#$ipRange = "192.168.1.1-254"
+
+#$ips = $ipRange.Split("-")
+#$startIP = [System.Net.IPAddress]::Parse($ips[0])
+#$endIP = [System.Net.IPAddress]::Parse($ips[1])
+#$ipAddresses = [System.Net.IPAddress]::GetAddressBytes($startIP)..[System.Net.IPAddress]::GetAddressBytes($endIP) | % {
+ #   [System.Net.IPAddress]::New($_[0], $_[1], $_[2], $_[3])
+#}
+
+#$liveHosts = @()
+#$ipAddresses | % {
+ #   $host = $_.ToString()
+   # if (Test-Connection -ComputerName $host -Count 1 -Quiet) {
+    #    $liveHosts += $host
+   # }
+#}
+
+# $liveHosts
